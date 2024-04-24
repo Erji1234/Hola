32f9f0b78b57efcabb2e12b39f93e0b3460d22e9
ad985 <ad985@MSI>
2024-04-24 12:01:42 +0200

    hola

--- Entornos3/src/Entornos/SumaNumeros.java
+++ Entornos3/src/Entornos/SumaNumeros.java
@@ -1,5 +1,23 @@
 package Entornos;
+	import java.util.Scanner;
 
-public class SumaNumeros {
+	public class SumaNumeros {
+	
+	
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        
+	        System.out.println("Ingrese el primer número:");
+	        int numero1 = scanner.nextInt();
+	        
+	        System.out.println("Ingrese el segundo número:");
+	        int numero2 = scanner.nextInt();
+	        
+	        int suma = numero1 + numero2;
+	        System.out.println("La suma de los números es: " + suma);
+	        
+	        scanner.close();
+	    }
+	}
 
-}
+
